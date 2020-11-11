01f15e1910b294a1e567ae73f5f2c9974c81b4e6
Miguel Zegarra <42479401+AckerCoder@users.noreply.github.com>
2020-11-10 22:10:12 -0500

    There is a bug with the round button, it does not display it

--- app/src/main/java/com/example/simple/ProductKardex.kt
+++ app/src/main/java/com/example/simple/ProductKardex.kt
@@ -2,10 +2,19 @@ package com.example.simple
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
+import android.widget.TextView
 
 class ProductKardex : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product_kardex)
+
+        val title = findViewById<TextView>(R.id.productTitle)
+        val sectionListView = findViewById<ListView>(R.id.sectionListView)
+
+
+        title.text = currentProduct?.name
+
     }
 }
